24a01493a1155e4ea8f4b3da9600a8f628ee5a61
olatuno <olatunde.olafimihan@outlook.com>
2025-11-04 09:24:04 +0000

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to CareConnect UK</h1>
+      <p>Empowering care providers and families with digital health tools.</p>
+    </div>
+  );
+}
+
+export default App;
